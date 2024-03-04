f2111f17049480278617ad0e811695c3d25dd5be
Lee Jaehyeon <wogusn@gmail.com>
2024-03-05 00:40:47 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -4,7 +4,7 @@ import feedparser
 blog_url = "https://versatile0010.github.io/feed.xml"
 rss_feed = feedparser.parse(blog_url)
 
-MAX_NUM = 5
+MAX_NUM = 3
 
 latest_posts = ""
 
@@ -47,7 +47,7 @@ woguns@gmail.com
 ```
 
 ---
-📝 **Latested Blog post...**
+📝 **Latested Blog posts...**
 
 """
 
